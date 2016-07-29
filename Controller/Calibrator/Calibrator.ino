1416e030ee3976d3431193ae1f8dd1196c2c792e
Ryan <ryanf.rowe@gmail.com>
2016-07-29 18:57:32 -0400

    I was so naive back then

--- Controller/Calibrator/Calibrator.ino
+++ Controller/Calibrator/Calibrator.ino
@@ -1,99 +1,90 @@
-//This sketch is used to find calibration values for the controller
-//it averages calibration values over a series of time
-
 #include <Constants.h>
 
-int rightLeftOffset, frontBackOffset, yawOffset, throttleOffset;
+/**
+ * This sketch is used to find calibration values for the controller.
+ * It averages calibration values over a series of time.
+ */
+
+int rollOffset, pitchOffset, yawOffset, throttleOffset;
+// The number of samples to average
+const int AVG_LENGTH = 50;
 
 void setup() {
   Serial.begin(9600);
   
-  //pin modes
-  pinMode(THR_JOYSTICK,INPUT);
-  pinMode(YAW_JOYSTICK,INPUT);
-  pinMode(RL_JOYSTICK,INPUT);
-  pinMode(FB_JOYSTICK,INPUT);
-  pinMode(POT,INPUT);
-  pinMode(TOGGLE,INPUT);
-  pinMode(SPEAKER_PIN,OUTPUT);
+  pinMode(THR_JOYSTICK, INPUT);
+  pinMode(YAW_JOYSTICK, INPUT);
+  pinMode(ROLL_JOYSTICK, INPUT);
+  pinMode(PITCH_JOYSTICK, INPUT);
+  pinMode(POT, INPUT);
+  pinMode(TOGGLE, INPUT);
+  pinMode(SPEAKER_PIN, OUTPUT);
   
+  // delay, why? who knows
   delay(1000);
   
+  // tone while calibrating
   tone(SPEAKER_PIN, 220);
   
-  //YAW
-  int sum = 0;
-  for(int x = 0; x < 50; x ++) {
-    sum += calibrate(YAW_JOYSTICK); 
-  }
-  yawOffset = sum / 50;
+  average(yawOffset, YAW_JOYSTICK);
+  average(rollOffset, ROLL_JOYSTICK);
+  average(pitchOffset, PITCH_JOYSTICK);
   
-  //RL
-  sum = 0;
-  for(int x = 0; x < 50; x ++) {
-    sum += calibrate(RL_JOYSTICK); 
-  }
-  rightLeftOffset = sum / 50;
-  
-  //FB
-  sum = 0;
-  for(int x = 0; x < 50; x ++) {
-    sum += calibrate(FB_JOYSTICK); 
-  }
-  frontBackOffset = sum / 50;
-  
-  Serial.print(" YAW: ");
-  Serial.print(yawOffset);
-  Serial.print(" RL: ");
-  Serial.print(rightLeftOffset);
-  Serial.print(" FB: ");
-  Serial.println(frontBackOffset);
-  delay(1000);
+  Serial.println("-------------- Calibration --------------");
+  Serial.print("YAW: " + yawOffset);
+  Serial.print("\tROLL: " + rollOffset);
+  Serial.println("\tPITCH: " + pitchOffset);
+  Serial.println("------------ End Calibration ------------");
+  Serial.println();
   
   noTone(SPEAKER_PIN);
+  delay(1000);
 }
 
 void loop() {
-  int RL = analogRead(RL_JOYSTICK);
-  int FB = analogRead(FB_JOYSTICK);
-  int YAW = analogRead(YAW_JOYSTICK);
-  int THROTTLE = analogRead(THR_JOYSTICK);
-  int POTReading = analogRead(POT);
-  int toggle = digitalRead(TOGGLE);
-  Serial.print("RL:");
-  Serial.print(RL);
-  Serial.print(" FB:");
-  Serial.print(FB);
-  Serial.print(" YAW:");
-  Serial.print(YAW);
-  Serial.print(" THR:");
-  Serial.print(THROTTLE);
-  Serial.print(" POT:");
-  Serial.print(POTReading);
-  Serial.print(" TOG:");
-  Serial.println(toggle);
-  delay(50);
+  Serial.print("ROLL:" + analogRead(ROLL_JOYSTICK));
+  Serial.print("\tPITCH: " + analogRead(PITCH_JOYSTICK));
+  Serial.print("\tYAW: " + analogRead(YAW_JOYSTICK));
+  Serial.print("\tTHR: " + analogRead(THR_JOYSTICK));
+  Serial.print("\tPOT: " + analogRead(POT));
+  Serial.println("\tTOG: " + digitalRead(TOGGLE));
+  delay(100);
 }
 
-int calibrate(int pin) {
-  int count = 0;
+/**
+ * Comutes the average value of a pin
+ * for AVG_LENGTH samples.
+ * 
+ * @param &avgValue  The average value of the pin
+ * @param pin        The pin to sample.
+ */
+int average(int &avgValue, int pin) {
   int sum = 0;
+  for(int i = 0; i < AVG_LENGTH; i++) {
+    sum += waitForTen(pin); 
+  }
+  avgValue = sum / AVG_LENGTH;
+}
+
+/**
+ * Samples a pin and returns a value only
+ * when it has been the same for 10 consecutive
+ * samples.
+ */ 
+int waitForTen(int pin) {
+  int count = 0;
   //get first val
-  int last = analogRead(pin);
-  delay(10);
+  int last = 0;
   
-  //stop if count is 10 or more
   while(count < 10) {
-    //get current val
     int current = analogRead(pin);
-    //increment count if they're the same
+
     if(current == last) {
       count++;
-      sum+=current;
+    } else {
+      count = 0;
     }
     
     last = current;
   }
-  
-  return sum / count;
 }
